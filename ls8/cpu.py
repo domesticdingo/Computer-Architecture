8199d91395049d1b469dc7f12294cd01111528d7
Indigo Richards <thedomesticdingo@gmail.com>
2020-05-22 10:15:49 -0700

    Sprint Challenge
    Refactoring + MVP

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -4,29 +4,42 @@ import sys, re
 
 class CPU:
     def __init__(self):
-        self.ram = [0] * 256
-        self.reg = [0] * 8
-        self.pc = 0
-        self.ir = 0
-        self.sp = 7
-        self.reg[self.sp] = 0xF4
+        self.ram = [0] * 256     #RAM 00-FF
+        self.reg = [0] * 8       #Registers
+        self.pc = 7              #Program counter location
+        self.sp = 6              #Stack pointer location
+        self.fl = 5              #Flag status location
+        self.reg[self.pc] = 0    #Init program counter
+        self.reg[self.sp] = 0xF4 #Init stack pointer
+        self.reg[self.fl] = 0    #Init flag status
+
         self.halted = False
+
         self.instruction = {
-            0x01: self.HLT,
-            0x82: self.LDI,
-            0x47: self.PRN,
-            0xA2: self.MUL,
-            0x45: self.PUSH,
-            0x46: self.POP,
-            0x50: self.CALL,
-            0x11: self.RET,
-            0xA0: self.ADD,
+            0x01: (self.HLT, 1),
+            0x82: (self.LDI, 3),
+            0x47: (self.PRN, 2),
+            0x45: (self.PUSH, 2),
+            0x46: (self.POP, 2),
+            0x50: (self.CALL, 0),
+            0x11: (self.RET, 0),
+            #Math
+            0xA0: (lambda: self.alu('ADD'), 3),
+            0xA1: (lambda: self.alu('SUB'), 3),
+            0xA2: (lambda: self.alu('MUL'), 3),
+            0xA3: (lambda: self.alu('DIV'), 3),
+            0x65: (lambda: self.alu('INC'), 2),
+            0x66: (lambda: self.alu('DEC'), 2),
+            #Sprint instructions
+            0xA7: (lambda: self.alu('CMP'), 3),
+            0x54: (self.JMP, 0),
+            0x55: (lambda: self.alu('JEQ'), 0),
+            0x56: (lambda: self.alu('JNE'), 0),
         }
 
     def load(self, program):
-        instructions = []
-        address = 0
         with open(f'examples/{program}.ls8', 'r') as punchcard:
+            address = 0
             for line in punchcard:
                 instruction = re.match(r'(\d+)(?=\D)', line) if re.match(r"(\d+)(?=\D)", line) else None
                 if instruction:
@@ -40,80 +53,97 @@ class CPU:
     def ram_write(self, MAR, MDR):
         self.ram[MAR] = MDR
 
-    def alu(self, op, reg_a, reg_b):
-        """ALU operations."""
+    def alu(self, operation):
+        #We doin math
+        def ADD():
+            self.reg[self.ram_read(self.reg[self.pc]+1)] += self.reg[self.ram_read(self.reg[self.pc]+2)]
+        def SUB():
+            self.reg[self.ram_read(self.reg[self.pc]+1)] -= self.reg[self.ram_read(self.reg[self.pc]+2)]
+        def MUL():
+            self.reg[self.ram_read(self.reg[self.pc]+1)] *= self.reg[self.ram_read(self.reg[self.pc]+2)]
+        def DIV():
+            self.reg[self.ram_read(self.reg[self.pc]+1)] /= self.reg[self.ram_read(self.reg[self.pc]+2)]
+        def INC():
+            self.reg[self.ram_read(self.reg[self.pc]+1)] += 1
+        def DEC():
+            self.reg[self.ram_read(self.reg[self.pc]+1)] -= 1
+        def CMP():
+            if self.reg[self.ram_read(self.reg[self.pc]+1)] == self.reg[self.ram_read(self.reg[self.pc]+2)]: self.reg[self.fl] = 0x01
+            elif self.reg[self.ram_read(self.reg[self.pc]+1)] < self.reg[self.ram_read(self.reg[self.pc]+2)]: self.reg[self.fl] = 0x04
+            else: self.reg[self.fl] = 0x02
 
-        if op == "ADD":
-            self.reg[reg_a] += self.reg[reg_b]
-        elif op == "SUB":
-            self.reg[reg_a] -= self.reg[reg_b]
-        elif op == "MUL":
-            self.reg[reg_a] *= self.reg[reg_b]
-        else:
+        #Not math
+        def JEQ(): #If equals flag is true, jump to address stored in given register
+            if self.reg[self.fl] == 0x01: self.reg[self.pc] = self.reg[self.ram_read(self.reg[self.pc]+1)]
+            else: self.reg[self.pc] += 2
+        def JNE(): #If equals flag is false, jump to address stored in given register
+            if self.reg[self.fl] != 0x01: self.reg[self.pc] = self.reg[self.ram_read(self.reg[self.pc]+1)]
+            else: self.reg[self.pc] += 2
+
+        #Run the thing
+        operations = {
+            'ADD': ADD,
+            'SUB': SUB,
+            'MUL': MUL,
+            'DIV': DIV,
+            'INC': INC,
+            'DEC': DEC,
+            'CMP': CMP,
+            'JEQ': JEQ,
+            'JNE': JNE,
+        }
+
+        try:
+            operations[operation]()
+        except:
             raise Exception("Unsupported ALU operation")
+
+    #Having fun being a computer
+    def LDI(self):
+        self.reg[self.ram_read(self.reg[self.pc]+1)] = self.ram_read(self.reg[self.pc]+2)
+
+    def PRN(self):
+       print(self.reg[self.ram_read(self.reg[self.pc]+1)])
+
+    def HLT(self):
+        self.halted = True
+
+    def PUSH(self, MDR = None):
+        self.reg[self.sp] -= 1
+        data = MDR if MDR else self.reg[self.ram[self.reg[self.pc]+1]]
+        self.ram_write(self.reg[self.sp], data)
+
+    def POP(self):
+        self.reg[self.ram_read(self.reg[self.pc]+1)] = self.ram_read(self.reg[self.sp])
+        self.reg[self.sp] += 1
+        
+    def CALL(self):
+        self.PUSH(self.reg[self.pc]+2)
+        self.reg[self.pc] = self.reg[self.ram_read(self.reg[self.pc]+1)]
+
+    def RET(self):
+        self.reg[self.pc] = self.ram_read(self.reg[self.sp])
+        self.reg[self.sp] += 1
+
+    def JMP(self):
+        self.reg[self.pc] = self.reg[self.ram_read(self.reg[self.pc]+1)]
 
     def trace(self):
         """
         Handy function to print out the CPU state. You might want to call this
         from run() if you need help debugging.
         """
-
-        print(f"TRACE: %02X | %02X %02X %02X |" % (
-            self.pc,
-            #self.fl,
-            #self.ie,
-            self.ram_read(self.pc),
-            self.ram_read(self.pc + 1),
-            self.ram_read(self.pc + 2)
+        print(f"TRACE: %02i | %03i %03i %03i |" % (
+            self.reg[self.pc]+1,
+            self.ram_read(self.reg[self.pc]),
+            self.ram_read(self.reg[self.pc] + 1),
+            self.ram_read(self.reg[self.pc] + 2)
         ), end='')
-
-        for i in range(8):
-            print(" %02X" % self.reg[i], end='')
-
+        for i in range(8): print(" %03i" % self.reg[i], end='')
         print()
 
-    def LDI(self):
-        self.reg[self.ram_read(self.pc+1)] = self.ram_read(self.pc+2)
-        self.pc += 3
-
-    def PRN(self):
-        print(self.reg[self.ram_read(self.pc+1)])
-        self.pc += 2
-
-    def HLT(self):
-        self.halted = True
-        self.pc += 1
-
-    def ADD(self):
-        self.alu('ADD', self.ram_read(self.pc+1),self.ram_read(self.pc+2))
-        self.pc += 3
-
-    def MUL(self):
-        reg_a = self.ram_read(self.pc+1)
-        reg_b = self.ram_read(self.pc+2)
-        self.alu('MUL', reg_a, reg_b)
-        self.pc += 3
-
-    def PUSH(self, MDR = None):
-        self.reg[self.sp] -= 1
-        data = MDR if MDR else self.reg[self.ram[self.pc+1]]
-        self.ram_write(self.reg[self.sp], data)
-        self.pc += 2
-
-    def POP(self):
-        self.reg[self.ram_read(self.pc+1)] = self.ram_read(self.reg[self.sp])
-        self.pc += 2
-        self.reg[self.sp] += 1
-        
-    def CALL(self):
-        self.PUSH(self.pc+2)
-        self.pc = self.reg[self.ram_read(self.pc-1)]
-
-    def RET(self):
-        self.pc = self.ram_read(self.reg[self.sp])
-        self.reg[self.sp] += 1
-        
     def run(self):
         while not self.halted:
-            IR = self.ram_read(self.pc)
-            self.instruction[IR]()
+            IR = self.ram_read(self.reg[self.pc])
+            self.instruction[IR][0]()
+            self.reg[self.pc] += self.instruction[IR][1]
